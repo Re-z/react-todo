9476b34d9494ee912616335ba41f13913af0510b
re-z <re-z@ukr.net>
2020-10-08 20:06:17 +0300

    updated

--- src/components/List.js
+++ src/components/List.js
@@ -7,15 +7,9 @@ class List extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			items: [],
-			inputValue: ''
+			items: [{name: 'test name', done: false, id: Date.now() }],
+			inputValue: '',
 		}
-		this.deleteItem = this.deleteItem.bind(this)
-		this.ItemsList = this.state.items.map(el => {
-			return (
-				<ListItem key={el} name={el} />
-			)
-		})
 	}
 	setInputValue = (ev) => {
 		const inputValue = ev.target.value;
@@ -23,18 +17,27 @@ class List extends React.Component {
 			inputValue: inputValue
 		})
 	}
-	deleteItem = () => {
-		console.log(this);
+	deleteItem = (el) => {
+		const newItems = this.state.items.filter(item => item.id != el.id )
+		this.setState({ items: newItems })
 	}
 	addItem = () => {
+		if(this.state.inputValue) {
+			const newItems = [...this.state.items];
+			newItems.push({name: this.state.inputValue, done: false, id: Date.now()});
+			this.setState({
+				items: newItems,
+				inputValue: ''
+			})
+		}
+	}
+	doneItems() { return this.state.items.filter(el =>  el.done === true) }
+	todoItems() { return this.state.items.filter(el =>  el.done === false) }
+	switchStatus = (el) => {
+		const index = this.state.items.indexOf(el);
 		const newItems = [...this.state.items];
-		newItems.push(this.state.inputValue);
-		this.setState({
-			items: newItems,
-			inputValue: ''
-		}, () => {
-			//callback
-		})
+		newItems[index].done = !newItems[index].done;
+		this.setState({items: newItems}) //перерендеривается все items-ы? Есть более оптимальный способ
 	}
 	render() {
 		return (
@@ -49,14 +52,28 @@ class List extends React.Component {
 					<button onClick={this.addItem} className="list-btn">Add item</button>
 				</div>
 				<ul className="list-box">
-					{
-						this.state.items.map(el => {
-							return (
-								<ListItem key={el} name={el} deleteItem={this.deleteItem.bind(this)}/>
+					{	//for loops?
+						this.state.items.length
+						? (this.state.items.map(el => {
+								return (
+									<ListItem 
+										key={el.id}
+										id={el.id}
+										name={el.name}
+										done={el.done}
+										deleteItem={() => this.deleteItem(el)}
+										switchStatus={() => this.switchStatus(el)}
+									/>
 							)
-						})
+						}))
+						: (<div>There is no items TODO</div>)
 					}
 				</ul>
+				<div>
+					<p>Total items: {this.state.items.length}</p>
+					<p>Done items: {this.doneItems().length}</p>
+					<p>Todo items: {this.todoItems().length}</p>
+				</div>
 			</div>
 			
 		)
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 
-export default ({deleteItem, name}) => {
+export default props => {
+	const liStyle = props.done ? {backgroundColor: 'green'} : {backgroundColor: 'red'};
 	return (
-		<li className="list-item">
-			<span>{name}</span>
-			<button onClick={deleteItem}>close</button>
+		<li className="list-item" style={liStyle}>
+			<span>{props.name}</span>
+			<span>id: {props.id}</span>
+			<span>
+				<button onClick={props.switchStatus}>switch status</button>
+				<button onClick={props.deleteItem}>close</button>
+			</span>
 		</li>
 	)
 }
